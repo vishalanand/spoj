17569049a0d3a0edc14bc580cfbe558366817ca9
Vishal Anand <anand.vishal2994@gmail.com>
2015-11-28 21:58:10 +0530

    Life, Universe, Everything

---
+++ TEST.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  int n;
+  while(cin >> n)
+  {
+    if(n == 42)
+      break;
+    cout << n << endl;
+  }
+  return 0;
+}
