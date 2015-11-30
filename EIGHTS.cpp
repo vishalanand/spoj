a8a8104e495381508fe5eee4b5301ce531c02ccb
Vishal Anand <anand.vishal2994@gmail.com>
2015-11-30 21:53:28 +0530

    Triple Fat Ladies

---
+++ EIGHTS.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  long long int t, k;
+  cin >> t;
+  while(t --)
+  {
+    cin >> k;
+    cout << (k - 1) * 250 + 192 << endl;
+  }
+  return 0;
+}
